bc79a4d325a0d3ff879be8822370a71d3d9358ce
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-13 00:32:46 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -3,17 +3,19 @@
 const moduleTest = require("../");
 
 var message1 = {};
-message1.manifest = {};
-message1.manifest.input = {
+message1 = {code:5, name:"name1"};
+
+var context = {};
+context.manifestObj = {};
+context.manifestObj.input = {
     type: "object",
     properties: {
         code: {type: "number", maximum: 5},
         name: {type: "string"}
     }
 };
-message1.body = {code:1, name:"name1"};
 
-moduleTest(message1).then(function(result){
+moduleTest.process(message1, context).then(function(result){
     console.log("sucess! ", result);
 }, function(err){
     console.log("err! ", err);
--- test/test.js
+++ test/test.js
@@ -4,17 +4,19 @@ const assert = require("assert");
 const moduleTest = require("../");
 
 var message1 = {};
-message1.manifest = {};
-message1.manifest.input = {
+message1 = {code:6, name:"name1"};
+
+var context = {};
+context.manifestObj = {};
+context.manifestObj.input = {
     type: "object",
     properties: {
         code: {type: "number", maximum: 5},
         name: {type: "string"}
     }
 };
-message1.body = {code:6, name:"name1"};
 
-moduleTest(message1).then(function(result){
+moduleTest.process(message1, context).then(function(result){
     assert.strictEqual(typeof(result), "null");
 }, function(err){
     assert.strictEqual(err.code, -32602);
